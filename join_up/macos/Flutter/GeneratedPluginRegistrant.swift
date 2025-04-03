efb74052851d02c6af36784d4a6f81b78324b650
ced3j <beteljesed@gmail.com>
2025-04-03 14:32:58 +0300

    generated plugin cakisma

--- join_up/macos/Flutter/GeneratedPluginRegistrant.swift
+++ join_up/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,5 +12,3 @@ import path_provider_foundation
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-}
