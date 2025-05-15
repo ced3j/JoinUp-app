ccbca457c1bcb88e579171462190a8a3c2d4d2ec
ced3j <beteljesed@gmail.com>
2025-05-15 15:39:43 +0300

    last updates

--- join_up/lib/favorite_event_screen.dart
+++ join_up/lib/favorite_event_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-class FavoritesPage  extends StatefulWidget {
-  final Set<int> favorites ;
-  final Function(int) toggleFavori;
+class FavoritesPage extends StatefulWidget {
+  final Set<String> favorites; // favori etkinliklerin id'lerini tutuyoruz
+  final Function(String) toggleFavori; // eventId parametresini alacak
 
   const FavoritesPage({
     super.key,
-    required this.favorites ,
-
+    required this.favorites,
     required this.toggleFavori,
   });
 
@@ -19,35 +18,34 @@ class _FavorilerSayfasiState extends State<FavoritesPage> {
   @override
   Widget build(BuildContext context) {
     const Color primaryColor = Color(0xFF6F2DBD); // Mor ton
-    
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Favori Etkinlikler', style: TextStyle(color: Colors.white)),
+        title: const Text(
+          'Favori Etkinlikler',
+          style: TextStyle(color: Colors.white),
+        ),
         iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: primaryColor,
-        ),
+      ),
       body: ListView(
-        children: widget.favorites .map((index) {
-
-          return Card(
-            child: ListTile(
-              leading: Icon(
-                Icons.star,
-                color: Colors.amber,
-              ),
-              title: Text("Etkinlik Başlığı $index"),
-              subtitle: Text("Açıklama $index"),
-              trailing: IconButton(
-                icon: const Icon(Icons.remove_circle, color: Colors.red),
-                onPressed: () {
-                  widget.toggleFavori(index); // ana sayfadaki favoriler güncelleniyor
-                  setState(() {}); // bu sayfa da güncellensin
-
-                },
-              ),
-            ),
-          );
-        }).toList(),
+        children:
+            widget.favorites.map((eventId) {
+              return Card(
+                child: ListTile(
+                  leading: Icon(Icons.star, color: Colors.amber),
+                  title: Text("Etkinlik Başlığı $eventId"),
+                  subtitle: Text("Açıklama $eventId"),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.remove_circle, color: Colors.red),
+                    onPressed: () {
+                      widget.toggleFavori(eventId); // Favoriden çıkar
+                      setState(() {});
+                    },
+                  ),
+                ),
+              );
+            }).toList(),
       ),
     );
   }
